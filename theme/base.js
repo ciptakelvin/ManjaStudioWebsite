04d888e8584d4603a50afaecfd351dcda772cb0d
Kelvin Cipta <ciptakelvin@gmail.com>
2024-11-06 14:26:08 +0700

    Sidebar

---
+++ theme/base.js
@@ -0,0 +1,21 @@
+// window.onscroll = function(){
+//     var scrollPosition = document.documentElement.scrollTop;
+//     if (scrollPosition<100) {
+
+//     }
+    
+// }
+$(document).scroll(function () { 
+    var scroll = Math.floor($(window).scrollTop()/300);
+    $("#sidebar h2").css("color","#9e9e9e")
+    $("#sidebar p").css("color","#9e9e9e")
+    $("#sidebar h2").eq(scroll).css("color","white")
+    $("#sidebar p").eq(scroll).css("color","white")
+});
+
+$(document).ready(function () {
+    $("#sidebar h2").css("color","#9e9e9e");
+    $("#sidebar p").css("color","#9e9e9e");
+    $("#sidebar h2").eq(0).css("color","white");
+    $("#sidebar p").eq(0).css("color","white");
+});
