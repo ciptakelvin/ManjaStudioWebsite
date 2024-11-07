e26553236b8916dc83ec7dcc11cf96b813e70578
Kelvin Cipta <ciptakelvin@gmail.com>
2024-11-07 08:16:51 +0700

    Autohide sidebar

--- theme/base.js
+++ theme/base.js
@@ -5,17 +5,26 @@
 //     }
     
 // }
-$(document).scroll(function () { 
-    var scroll = Math.floor($(window).scrollTop()/400);
-    $("#sidebar h2").css("color","#9e9e9e")
-    $("#sidebar p").css("color","#9e9e9e")
-    $("#sidebar h2").eq(scroll).css("color","white")
-    $("#sidebar p").eq(scroll).css("color","white")
-});
-
 $(document).ready(function () {
+    $("#sidebar").hide();
     $("#sidebar h2").css("color","#9e9e9e");
     $("#sidebar p").css("color","#9e9e9e");
     $("#sidebar h2").eq(0).css("color","white");
     $("#sidebar p").eq(0).css("color","white");
+});
+
+
+$(document).scroll(function () { 
+    $("#sidebar").fadeIn();
+    clearInterval(interval);
+    interval = setInterval(() => {
+        $("#sidebar").fadeOut();
+        didScroll = false;
+        console.log('Someone scrolled me!')
+    }, 500);
+    var scroll = Math.floor($(window).scrollTop()/400);
+    $("#sidebar h2").css("color","#9e9e9e");
+    $("#sidebar p").css("color","#9e9e9e");
+    $("#sidebar h2").eq(scroll).css("color","white");
+    $("#sidebar p").eq(scroll).css("color","white");
 });
